7072e99087ca27b639e10be06e1750376d540543
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 13:34:39 +0530

    Finished Activity2.java code

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/example_01/Activity2.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/example_01/Activity2.java
@@ -2,15 +2,29 @@ package me.mtron.mobile_application_development_labsheet3.example_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class Activity2 extends AppCompatActivity {
 
+    TextView name;
+    TextView address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        name = findViewById(R.id.nameText);
+        address = findViewById(R.id.addressText);
+
+        Intent content = getIntent();
+        Bundle get = content.getExtras();
+
+        name.setText(get.getString("NAME"));
+        address.setText(get.getString("ADDRESS"));
     }
 }
