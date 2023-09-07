4d34e2bf17b2c5641735984c4394499d8d27618a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 15:46:37 +0530

    Finished exercise_01_bundle_object Activity3.java

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity3.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity3.java
@@ -2,15 +2,35 @@ package me.mtron.mobile_application_development_labsheet3.exercise_01_bundle_obj
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class Activity3 extends AppCompatActivity {
 
+    TextView nameText;
+    TextView indexText;
+    TextView departmentText;
+    TextView facultyText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_7);
+
+        nameText = findViewById(R.id.nameText2);
+        indexText = findViewById(R.id.indexNumText2);
+        departmentText = findViewById(R.id.departmentText2);
+        facultyText = findViewById(R.id.facultyText2);
+
+        Intent content = getIntent();
+        Bundle get = content.getExtras();
+
+        nameText.setText(get.getString("NAME"));
+        indexText.setText(get.getString("INDEX"));
+        departmentText.setText(get.getString("DEPARTMENT"));
+        facultyText.setText(get.getString("FACULTY"));
     }
 }
