d875b34d8ed0359e787b274edf1f9ef9334bdfcd
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 13:44:23 +0530

    Created Activity1.java code

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/example_02/Activity1.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet3.example_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet3.R;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+    }
+}
