d69e1caf450d78ed4b1eaeead8a7713e407198f2
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 13:25:12 +0530

    Finished Activity1.java code

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/example_01/Activity1.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/example_01/Activity1.java
@@ -2,15 +2,31 @@ package me.mtron.mobile_application_development_labsheet3.example_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class Activity1 extends AppCompatActivity {
 
+    EditText name;
+    EditText address;
+    Button login;
+    Bundle bundle;
+    Intent check;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.submitBtn);
+        name = findViewById(R.id.nameTxt);
+        address = findViewById(R.id.addressTxt);
+
+        check = new Intent(this, Activity2.class);
+        bundle = new Bundle();
     }
 }
