c69d73c38854ff48bf693fe8a8a2409fe762834b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 19:09:22 +0530

    Finished LastActivity.java

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_03_shared_preferences/LastActivity.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_03_shared_preferences/LastActivity.java
@@ -2,15 +2,30 @@ package me.mtron.mobile_application_development_labsheet3.exercise_03_shared_pre
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class LastActivity extends AppCompatActivity {
 
+    TextView name;
+    TextView city;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_last);
+
+        name = findViewById(R.id.text1);
+        city = findViewById(R.id.text22);
+
+        SharedPreferences data2 = getSharedPreferences("data", MODE_PRIVATE);
+        String _name = data2.getString("NAME", "NAME not stored");
+        String _city = data2.getString("CITY", "CITY not stored");
+
+        name.setText(_name);
+        city.setText(_city);
     }
 }
