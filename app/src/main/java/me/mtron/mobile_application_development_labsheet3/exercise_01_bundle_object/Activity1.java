8385ae82d678b94c6c8787b5bbcd010a04f8b943
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 15:23:51 +0530

    Finished exercise_01_bundle_object Activity1.java

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity1.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity1.java
@@ -2,9 +2,12 @@ package me.mtron.mobile_application_development_labsheet3.exercise_01_bundle_obj
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
@@ -14,6 +17,8 @@ public class Activity1 extends AppCompatActivity {
     EditText password;
     Button login;
 
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +27,17 @@ public class Activity1 extends AppCompatActivity {
         userName = findViewById(R.id.userNameText1);
         password = findViewById(R.id.passwordText1);
         login = findViewById(R.id.loginBtn);
-
+        intent = new Intent(this, Activity2.class);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(userName.getText().toString().equals("user") && password.getText().toString().equals("root")){
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(Activity1.this, "Invalid credentials.\nCheck your username and password!!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
