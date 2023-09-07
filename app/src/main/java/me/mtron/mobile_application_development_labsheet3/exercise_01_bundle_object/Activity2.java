a9d828d90d9e62a5964165c23531b353d405b098
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 15:33:58 +0530

    Working on exercise_01_bundle_object Activity2.java

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity2.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity2.java
@@ -3,14 +3,37 @@ package me.mtron.mobile_application_development_labsheet3.exercise_01_bundle_obj
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class Activity2 extends AppCompatActivity {
 
+    EditText nameText;
+    EditText indexText;
+    EditText departmentText;
+    EditText facultyText;
+    Button btnOk;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_6);
+
+        nameText = findViewById(R.id.nameText1);
+        indexText = findViewById(R.id.indexNumText1);
+        departmentText = findViewById(R.id.departmentText1);
+        facultyText = findViewById(R.id.facultyText1);
+        btnOk = findViewById(R.id.okBtn);
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
