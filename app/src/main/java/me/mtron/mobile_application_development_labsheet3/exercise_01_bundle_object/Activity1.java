b457cd795a445f0c23b648855697095086daa183
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 15:16:28 +0530

    Working on exercise_01_bundle_object

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity1.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity1.java
@@ -3,14 +3,26 @@ package me.mtron.mobile_application_development_labsheet3.exercise_01_bundle_obj
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class Activity1 extends AppCompatActivity {
 
+    EditText userName;
+    EditText password;
+    Button login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_5);
+
+        userName = findViewById(R.id.userNameText1);
+        password = findViewById(R.id.passwordText1);
+        login = findViewById(R.id.loginBtn);
+
+
     }
 }
