da493c8290e4d6072a2a7a832181b24638a8e0d3
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 18:43:13 +0530

    Finished exercise_03 SignIn.java

--- app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_03_shared_preferences/SignIn.java
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_03_shared_preferences/SignIn.java
@@ -2,15 +2,47 @@ package me.mtron.mobile_application_development_labsheet3.exercise_03_shared_pre
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import me.mtron.mobile_application_development_labsheet3.R;
 
 public class SignIn extends AppCompatActivity {
 
+    EditText userName;
+    EditText password;
+
+    Button signIn;
+
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        userName = findViewById(R.id.userNameText3);
+        password = findViewById(R.id.passwordText4);
+        signIn = findViewById(R.id.signInBtn);
+
+        intent = new Intent(this, LastActivity.class);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences data2 = getSharedPreferences("details", MODE_PRIVATE);
+                String uname = data2.getString("USERNAME", "USERNAME not stored");
+                String pwd = data2.getString("PASSWORD", "PASSWORD not stored");
+
+                if(uname.equals(userName.getText().toString()) && pwd.equals(password.getText().toString())){
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
