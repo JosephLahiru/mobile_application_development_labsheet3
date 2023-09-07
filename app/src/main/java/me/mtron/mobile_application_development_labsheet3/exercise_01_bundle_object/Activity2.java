b26a5efe2fcd30d6b53aae3a3f2bc029dd8f6fdd
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-09-07 15:08:10 +0530

    Started exercise_01_bundle_object

---
+++ app/src/main/java/me/mtron/mobile_application_development_labsheet3/exercise_01_bundle_object/Activity2.java
@@ -0,0 +1,16 @@
+package me.mtron.mobile_application_development_labsheet3.exercise_01_bundle_object;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.mtron.mobile_application_development_labsheet3.R;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_6);
+    }
+}
